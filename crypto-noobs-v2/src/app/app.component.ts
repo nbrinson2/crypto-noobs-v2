1c0b192d950951956230d3e8bbab261c96449555
Nick Brinson <whataretheodds10@gmail.com>
2021-11-03 09:49:30 -0400

    transfer

--- crypto-noobs-v2/src/app/app.component.ts
+++ crypto-noobs-v2/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'crypto-noobs-v2';
+  public title = 'crypto-noobs-v2';
+
+  public events: string[] = [];
+  public opened = true;
+  public mini = true;
+  public scrollY: number | undefined;
+  public showFiller: boolean = true;
+
+  // @HostListener('scroll', ['$event']) // for scroll events of the current element
+  @HostListener('window:scroll', ['$event']) // for window scroll events
+  onScroll(event: any) {
+    this.scrollY = window.scrollY;
+    console.log(window.scrollY); // Value of scroll Y in px
+  }
+
+  ngOnInit() {
+    window.addEventListener("scroll", this.scroll, true);
+  }
+
+
+
+  public scroll = () => {
+    const scrollPos = document.documentElement.scrollTop;
+
+    // change '50' according to when you want to change the image
+    if (scrollPos > 50) {
+      console.log('New image');
+    }
+    else {
+      console.log('Old Image');
+    }
+  }
+
+  public toggleSidebar() {
+    if (this.mini) {
+      document.getElementById("mySidebar").style.width = "250px";
+      document.getElementById("main").style.marginLeft = "250px";
+      this.mini = false;
+    } else {
+      document.getElementById("mySidebar").style.width = "85px";
+      document.getElementById("main").style.marginLeft = "85px";
+      this.mini = true;
+    }
+  }
+
+
+
+
 }
--- crypto-noobs-v2/src/app/app.module.ts
+++ crypto-noobs-v2/src/app/app.module.ts
@@ -1,15 +1,61 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
+import { ServicesComponent } from './services/services.component';
+import { ResourcesComponent } from './resources/resources.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { FooterComponent } from './footer/footer.component';
+import { FaqComponent } from './faq/faq.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatIconModule } from "@angular/material/icon";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatRadioModule } from "@angular/material/radio";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    ServicesComponent,
+    ResourcesComponent,
+    ContactUsComponent,
+    AboutUsComponent,
+    FooterComponent,
+    FaqComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatGridListModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatDividerModule,
+    MatRadioModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
